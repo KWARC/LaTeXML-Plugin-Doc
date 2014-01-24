62655b43f45c441d24f447eee42196a5463c0293
Deyan Ginev <d.ginev@jacobs-university.de>
2014-01-24 14:20:44 +0100

    adding a sample spine for an ODT Manifest generator

---
+++ lib/LaTeXML/Post/Manifest/ODT.pm
@@ -0,0 +1,110 @@
+# /=====================================================================\ #
+# |  LaTeXML::Post::Manifest::ODT                                       | #
+# | Manifest creation for EPUB                                          | #
+# |=====================================================================| #
+# | Part of LaTeXML:                                                    | #
+# |  Public domain software, produced as part of work done by the       | #
+# |  United States Government & not subject to copyright in the US.     | #
+# |---------------------------------------------------------------------| #
+# | Bruce Miller <bruce.miller@nist.gov>                        #_#     | #
+# | http://dlmf.nist.gov/LaTeXML/                              (o o)    | #
+# \=========================================================ooo==U==ooo=/ #
+package LaTeXML::Post::Manifest::ODT;
+use strict;
+use warnings;
+
+use base qw(LaTeXML::Post::Manifest);
+use LaTeXML::Util::Pathname;
+use File::Spec::Functions qw(catdir);
+use LaTeXML::Post;    # for error handling!
+
+sub new {
+  my ($class, %options) = @_;
+  my $self = $class->SUPER::new(%options);
+  return $self; }
+
+sub initialize {
+  my ($self, $doc) = @_;
+  my $directory = $$self{siteDirectory};
+  # Generate all auxiliary file and structure.
+  # E.g. for EPUB we do:
+  # 1. Create mimetype declaration
+  # 2.1. Add the container.xml description
+  # 3. Create OPS content container
+  # 3.1 OPS/content.opf XML Spine
+  # Metadata
+  # Fish out any existing unique identifier for the book
+  #       the UUID is the fallback default
+  # Manifest
+  # .. manifest generation
+  # To store the generated content (since we are not writing to disk here yet) use the Manifest object:
+    # $$self{OPS_directory} = $OPS_directory;
+    # $$self{opf}           = $opf;
+    # $$self{opf_spine}     = $spine;
+    # $$self{opf_manifest}  = $manifest;
+    # $$self{nav}           = $nav;
+    # $$self{nav_map}       = $nav_map;
+  return; }
+
+sub process {
+  my ($self, @docs) = @_;
+  $self->initialize($docs[0]);
+  foreach my $doc (@docs) {
+    # Add each document to the spine manifest
+    if (my $destination = $doc->getDestination) {
+      # my (undef, $name, $ext) = pathname_split($destination);
+      # my $file = "$name.$ext";
+      # my $relative_destination = pathname_relative($destination, $$self{OPS_directory});
+
+      # Add to manifest
+        # my $manifest = $$self{opf_manifest};
+        # my $item = $manifest->addNewChild(undef, 'item');
+        # $item->setAttribute('id',         $file);
+        # $item->setAttribute('href',       $relative_destination);
+        # $item->setAttribute('media-type', "application/xhtml+xml");
+        # my @properties;
+        # push @properties, 'mathml' if $doc->findnode('//*[local-name() = "math"]');
+        # push @properties, 'svg'    if $doc->findnode('//*[local-name() = "svg"]');
+        # my $properties = join(" ", @properties);
+        # $item->setAttribute('properties', $properties) if $properties;
+
+      # Add to spine
+        # my $spine = $$self{opf_spine};
+        # my $itemref = $spine->addNewChild(undef, 'itemref');
+        # $itemref->setAttribute('idref', $file);
+
+      # Add to navigation
+        # my $nav_map = $$self{nav_map};
+        # my $nav_li  = $nav_map->addNewChild(undef, 'li');
+        # my $nav_a   = $nav_li->addNewChild(undef, 'a');
+        # $nav_a->setAttribute('href', $file);
+        # $nav_a->appendText($file); } 
+  } }
+  $self->finalize;
+  return; }
+
+sub finalize {
+  my ($self) = @_;
+  # Write everything to disk:
+
+  #Index all CSS files (written already)
+  # Write the content.opf file to disk
+  # my $directory = $$self{siteDirectory};
+  # my $OPF_FH;
+  # my $content_path = pathname_concat($OPS_directory, 'content.opf');
+  # open($OPF_FH, ">", $content_path)
+  #   or Fatal('I/O', 'content.opf', undef, "Couldn't open '$content_path' for writing: $_");
+  # print $OPF_FH $$self{opf}->toString(1);
+  # close $OPF_FH;
+
+  # Write toc.ncx file to disk
+  # my $NAV_FH;
+  # my $nav_path = pathname_concat($OPS_directory, 'nav.xhtml');
+  # open($NAV_FH, ">", $nav_path)
+  #   or Fatal('I/O', 'nav.xhtml', undef, "Couldn't open '$nav_path' for writing: $!");
+  # print $NAV_FH $$self{nav}->toString(1);
+  # close $NAV_FH;
+
+  return (); }
+
+1;
