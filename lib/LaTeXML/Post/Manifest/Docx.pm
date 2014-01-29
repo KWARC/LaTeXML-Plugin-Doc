81096e36f5a6354b42a5525b95841a23b430cc01
Deyan Ginev <d.ginev@jacobs-university.de>
2014-01-29 17:03:12 +0100

    adding the document.xml.rels file

--- lib/LaTeXML/Post/Manifest/Docx.pm
+++ lib/LaTeXML/Post/Manifest/Docx.pm
@@ -18,6 +18,8 @@ use File::Spec::Functions qw(catdir catfile);
 use XML::LibXML;
 use LaTeXML::Util::Pathname;
 use LaTeXML::Post;    # for error handling!
+use LaTeXML::Post::XSLT;
+use LaTeXML::Post::Writer;
 
 sub new {
   my ($class, %options) = @_;
@@ -27,7 +29,6 @@ sub new {
 sub initialize {
   my ($self, $doc) = @_;
   my $directory = $$self{siteDirectory};
-  print STDERR "\n\n Initialize DOCS processing...\n\n";
   # Copy static files from ODT-Skeleton
   my $content_types = pathname_find('[Content_Types]',types=>['xml'],installation_subdir=>catdir('resources','WML-Skeleton'));
   my $skeleton_directory = pathname_directory($content_types);
@@ -36,23 +37,38 @@ sub initialize {
       mkdir(catdir($directory, $subdirectory)); }
     # Annoying, but let's copy each of the 6 static files individually:
     my @static_files = (
-      [ catfile(catdir($skeleton_directory,'docProps'),'app.xml'), 'docProps' ],
-      [ catfile(catdir($skeleton_directory,'docProps'),'core.xml'), 'docProps' ],
-      [ catfile(catdir($skeleton_directory,'word'),'fontTable.xml'), 'word' ],
-      [ catfile(catdir($skeleton_directory,'word'),'settings.xml'), 'word' ],
-      [ catfile(catdir($skeleton_directory,'word'),'styles.xml'), 'word' ],
+      [ catfile($skeleton_directory,'docProps','app.xml'), 'docProps' ],
+      [ catfile($skeleton_directory,'docProps','core.xml'), 'docProps' ],
+      [ catfile($skeleton_directory,'word','fontTable.xml'), 'word' ],
+      [ catfile($skeleton_directory,'word','settings.xml'), 'word' ],
+      [ catfile($skeleton_directory,'word','styles.xml'), 'word' ],
       [ catfile($skeleton_directory,'[Content_Types].xml'), '.']);
     foreach my $static_file(@static_files) {
       pathname_copy($static_file->[0], catdir($directory, $static_file->[1])); } }
   else {
     Error('I/O',$content_types,undef,"Couldn't find WML static resource: $content_types.xml"); }
   
-
+  my $document_rels = catfile($directory,'word','_rels','document.xml.rels');
+  # We need to create the document.xml.rels by applying a stylesheet to document.xml:
+  # TODO Rename relations.xsl to docx-relations.xsl
+  my $rels_stylesheet = LaTeXML::Post::XSLT->new(stylesheet => 'relations.xsl', noresources => 1);
+  my $rels_document = $rels_stylesheet->process($doc);
+  $rels_document->{destination} = $document_rels;
+  # TODO: Think about whether just using LibXML won't be simpler
+  my $writer = LaTeXML::Post::Writer->new(format=>'xml',omit_doctype=>0);
+  $writer->process($rels_document,$rels_document->getDocumentElement);
   return; }
 
 sub process {
   my ($self, @docs) = @_;
   $self->initialize($docs[0]);
+  # If needed: generate data from each of the @docs.
+  $self->finalize($docs[0]);
   return; }
+
+sub finalize {
+  my ($self,$doc) = @_;
+}
+
 
 1;
