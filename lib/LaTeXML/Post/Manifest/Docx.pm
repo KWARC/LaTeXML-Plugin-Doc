42694c334c771ffadc764760fdad7be64053aa9f
lkohlhase <lukas.kohlhase@nist.gov>
2014-08-05 13:34:22 -0400

    Merged code for docx and word into one document.

--- lib/LaTeXML/Post/Manifest/Docx.pm
+++ lib/LaTeXML/Post/Manifest/Docx.pm
@@ -32,6 +32,77 @@ sub new {
 
 sub initialize {
   my ($self, $xml) = @_;
+  my $bibnode = $xml->findnode('//ltx:bibliography');
+  if ($bibnode) {
+  	my $bibs=$bibnode->getAttribute('files'); #Find the bibliography that is used 
+  	if($bibs){
+  	print "\n \n \n \n Penis \n \n \n \n ";
+  	my $transform_stylesheet = LaTeXML::Post::XSLT-> new (stylesheet => 'tex2word.xsl', noresources=>1);
+  my $doc = $transform_stylesheet->process($xml); #Apply tex2word.xsl to the processed document.
+  my $directory = $$self{siteDirectory};
+  # Copy static files from ODT-Skeleton
+  my $content_types = pathname_find('[Content_Types]',types=>['xml'],installation_subdir=>catdir('resources','WML-Skeleton'));
+  my $skeleton_directory = pathname_directory($content_types);
+  if ($skeleton_directory) {
+    foreach my $subdirectory (qw/_rels word docProps customXML/, catdir('word','_rels'),catdir('customXML','_rels'),catdir('word','media')) { #create the file structure
+      mkdir(catdir($directory, $subdirectory)); }
+    # Annoying, but let's copy each of the 6 static files individually:
+    my @static_files = (
+      [ catfile($skeleton_directory,'docProps','app.xml'), catdir($directory,'docProps') ],
+      [ catfile($skeleton_directory,'docProps','core.xml'), catdir($directory,'docProps') ],
+      [ catfile($skeleton_directory,'word','fontTable.xml'), catdir($directory,'word') ],
+      [ catfile($skeleton_directory,'word','settings.xml'), catdir($directory,'word') ],
+      [ catfile($skeleton_directory,'word','styles.xml'), catdir($directory,'word') ],
+      [ catfile($skeleton_directory,'word','footnotes.xml'), catdir($directory,'word') ],
+      [ catfile($skeleton_directory,'customXML','_rels','item1.xml.rels'), catdir($directory,'customXML','_rels') ],
+      [ catfile($skeleton_directory,'customXML','itemProps1.xml'), catdir($directory,'customXML') ],
+      [ catfile($skeleton_directory,'_rels','.rels'), catdir($directory,'_rels') ],
+      [ catfile($skeleton_directory,'[Content_Types].xml'), catdir($directory,'.')]);
+    foreach my $static_file(@static_files) {
+      pathname_copy($static_file->[0],  $static_file->[1]); } } #populate the file structure
+  else {
+    Error('I/O',$content_types,undef,"Couldn't find WML static resource: $content_types.xml"); }
+  my $document_footnotes =catfile($directory,'word','footnotes.xml');
+  my $footnotes_stylesheet = LaTeXML::Post::XSLT-> new (stylesheet => 'docx-footnotes.xsl', noresources=>1);
+  my $footnotes_document = $footnotes_stylesheet->process($doc); #We create footnotes.xml by applying footnotes.xsl to document1.xml 
+  $footnotes_document->{destination}= $document_footnotes;
+  my $document_final =catfile($directory,'word','document.xml');
+  my $cleanup_stylesheet = LaTeXML::Post::XSLT->new (stylesheet => 'docx-cleaner.xsl', noresources => 1); 
+  my $final_document = $cleanup_stylesheet->process($doc); #We create document.xml by applying cleaner.xsl to document1.xml
+  $final_document->{destination} =$document_final;
+  my $document_rels = catfile($directory,'word','_rels','document.xml.rels');
+  my $rels_stylesheet = LaTeXML::Post::XSLT->new(stylesheet => 'docx-relations.xsl', noresources => 1);
+    # We need to create the document.xml.rels by applying a stylesheet to document1.xml:
+  my $rels_document = $rels_stylesheet->process($doc);
+  $rels_document->{destination} = $document_rels;
+  my $writer = LaTeXML::Post::Writer->new(format=>'xml',omit_doctype=>0);
+  $writer->process($rels_document,$rels_document->getDocumentElement);
+  $writer->process($final_document,$final_document->getDocumentElement); 
+  $writer->process($footnotes_document,$footnotes_document->getDocumentElement); 
+
+  my $current=cwd();                             
+  my $bibnode = $xml->findnode('//ltx:bibliography');
+  my $bibs=$bibnode->getAttribute('files');
+  my $bib=$bibs.'.bib'; #TODO Implement code to work with multiple bibliographies at once. Simply split at , and append files should work. 
+  my $bib_pathname= catfile(catdir($directory,'customXML'),'item1.xml');
+  my $cmd= "latexmlc $bib --destination=$bib_pathname";
+  system($cmd);
+  $xml->{destination}=catfile($directory,'paper.ltxml');
+  $writer->process($xml,$xml->getDocumentElement);
+  my $xslt = XML::LibXSLT->new();
+  my $source = XML::LibXML->load_xml(location =>$bib_pathname);
+  my $stylesheetus=catfile(catdir($skeleton_directory,'..','XSLT'),'docx-bibliographies.xsl'); 
+  my $style_doc = XML::LibXML->load_xml(location=>$stylesheetus);
+  my $stylesheet = $xslt->parse_stylesheet($style_doc); 
+  my $temp=catfile($directory,'paper.ltxml');
+  my $results = $stylesheet->transform($source, test =>"'$temp'");
+  $stylesheet->output_file($results,$bib_pathname);
+  unlink $temp;
+  	}
+ 
+  }
+  else {
+ 
   my $transform_stylesheet = LaTeXML::Post::XSLT-> new (stylesheet => 'tex2docx.xsl', noresources=>1);
   my $doc = $transform_stylesheet->process($xml); #Apply tex2docx.xsl to the processed document.
   my $directory = $$self{siteDirectory};
@@ -77,7 +148,8 @@ sub initialize {
   $writer->process($final_document,$final_document->getDocumentElement); 
   $writer->process($footnotes_document,$footnotes_document->getDocumentElement); 
 
-  my $current=cwd();                             
+  my $current=cwd();
+  }                             
   return; }
 
 sub process {
